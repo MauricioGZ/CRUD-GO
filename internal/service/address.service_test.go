b664d2404e541f7bb4211df79e898a593f2cef7d
Mauricio Andrés Gómez Zuluaga <mauricio.gomezzu@outlook.es>
2024-08-27 19:08:35 -0500

    missing file added

---
+++ internal/service/address.service_test.go
@@ -0,0 +1,103 @@
+package service
+
+import (
+	"context"
+	"testing"
+)
+
+func TestRegisterAddress(t *testing.T) {
+	testCases := []struct {
+		Name          string
+		Email         string
+		AddressType   string
+		Address       string
+		City          string
+		State         string
+		Country       string
+		ZipCode       string
+		ExpectedError error
+	}{
+		{
+			Name:          "RegisterAddress: Success",
+			Email:         "email@existinguser.com",
+			AddressType:   "billing",
+			Address:       "valid street",
+			City:          "valid city",
+			State:         "valid state",
+			Country:       "valid country",
+			ZipCode:       "0000001",
+			ExpectedError: nil,
+		},
+		{
+			Name:          "RegisterAddress: Success",
+			Email:         "email@doesntexist.com",
+			AddressType:   "billing",
+			Address:       "valid street",
+			City:          "valid city",
+			State:         "valid state",
+			Country:       "valid country",
+			ZipCode:       "0000001",
+			ExpectedError: ErrUserDoesntExist,
+		},
+	}
+
+	ctx := context.Background()
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			repo.Mock.Test(t)
+
+			err := s.RegisterAddress(
+				ctx,
+				tc.Email,
+				tc.AddressType,
+				tc.Address,
+				tc.City,
+				tc.State,
+				tc.Country,
+				tc.ZipCode,
+			)
+			if err != tc.ExpectedError {
+				t.Errorf("Expected error %v, got %v", tc.ExpectedError, err)
+			}
+		})
+	}
+}
+
+func TestGetAllAddresses(t *testing.T) {
+	testCases := []struct {
+		Name          string
+		Email         string
+		ExpectedError error
+	}{
+		{
+			Name:          "RegisterAddress: Success",
+			Email:         "email@existinguser.com",
+			ExpectedError: nil,
+		},
+		{
+			Name:          "RegisterAddress: Success",
+			Email:         "email@doesntexist.com",
+			ExpectedError: ErrUserDoesntExist,
+		},
+	}
+
+	ctx := context.Background()
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.Name, func(t *testing.T) {
+			t.Parallel()
+			repo.Mock.Test(t)
+
+			_, err := s.GetAllAddresses(ctx, tc.Email)
+			if err != tc.ExpectedError {
+				t.Errorf("Expected error %v, got %v", tc.ExpectedError, err)
+			}
+		})
+	}
+}
