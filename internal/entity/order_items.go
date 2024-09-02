076e2ae38a8a1dc3c749839cbec4c04005134967
Mauricio Andrés Gómez Zuluaga <mauricio.gomezzu@outlook.es>
2024-09-02 12:20:40 -0500

    order_item entity enhanced

--- internal/entity/order_items.go
+++ internal/entity/order_items.go
@@ -1,9 +1,18 @@
 package entity
 
+import "time"
+
 type OrderItem struct {
 	ID        int64
 	OrderID   int64
 	ProductID int64
 	Quantity  int64
 	Price     float32
+}
+
+type OrderItemByUserID struct {
+	OrderItem
+	OrderDate  time.Time
+	Status     string
+	TotalPrice float32
 }
